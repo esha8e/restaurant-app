f572420cb02c37f4aeec8dbe37b142cd963631ca
esha8e <eshae8889@gmail.com>
2022-07-08 00:55:56 +0530

    component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SearchRestaurantComponent } from './search-restaurant/search-restaurant.component';
+import { ViewfoodComponent } from './viewfood/viewfood.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    SearchRestaurantComponent
+    SearchRestaurantComponent,
+    ViewfoodComponent
   ],
   imports: [
     BrowserModule,
